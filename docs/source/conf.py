2f3c4944860dc36f394583ba8a8f2043979979d3
Maciej Krzywda <43749180+mkrzywda@users.noreply.github.com>
2021-05-26 09:51:09 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'sphinx_iowmd_theme'
-html_theme = 'sphinx_pdj_theme'
+html_theme = 'scrolls'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
