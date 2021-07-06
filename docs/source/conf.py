0a678fb87ad31ddaa9db7543bfff75b456207105
Maciej Krzywda <43749180+mkrzywda@users.noreply.github.com>
2021-07-06 13:08:30 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,8 +52,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_iowmd_theme'
-#html_theme = 'scrolls'
 html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
